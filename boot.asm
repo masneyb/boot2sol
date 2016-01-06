0cf1aefe158f302dc4150271203a053c2d9bde25
root <root@localhost.localdomain>
2016-01-06 14:35:45 -0500

    added subroutine for printing the bottom stacks

--- boot.asm
+++ boot.asm
@@ -16,14 +16,12 @@
 	mov al, 12h	; 12h = G  80x30  8x16  640x480   16/256K  .   A000 VGA,ATI VIP
 	int 10h		; Call BIOS
 
-	mov dh, 5d
-	mov dl, 10d
-	mov ax, card
- 	call print_card
+
 	call draw_board
 	jmp $
 
-
+	;; card location should be ax
+	;; does not modify cursor position
 print_card:
 	call fetch_card_value
 	cmp cl, 10d
@@ -159,7 +157,145 @@ draw_board:
 	cmp dl, 7d
 	jne .printdeck
 
+.print_bottom_piles:
+	mov dh, 11d
+	mov dl, 0d
+	mov al, '|'
+line1:
+	call print_char
+	inc dh
+	cmp dh, 21d
+	jne line1
+	mov dh, 11d
+	mov dl, 10d
+line2:
+	call print_char
+	inc dh
+	cmp dh, 21d
+	jne line2
+	mov dh, 11d
+	mov dl, 20d
+line3:
+	call print_char
+	inc dh
+	cmp dh, 21d
+	jne line3
+	mov dh, 11d
+	mov dl, 30d
+line4:
+	call print_char
+	inc dh
+	cmp dh, 21d
+	jne line4
+	mov dh, 11d
+	mov dl, 40d
+line5:
+	call print_char
+	inc dh
+	cmp dh, 21d
+	jne line5
+	mov dh, 11d
+	mov dl, 50d
+line6
+	call print_char
+	inc dh
+	cmp dh, 21d
+	jne line6
+	mov dh, 11d
+	mov dl, 60d
+line7:
+	call print_char
+	inc dh
+	cmp dh, 21d
+	jne line7
+	mov dh, 11d
+	mov dl, 70d
+line8:
+	call print_char
+	inc dh
+	cmp dh, 21d
+	jne line8
+	mov dh, 11d
+	mov dl, 80d
+line9:
+	call print_char
+	inc dh
+	cmp dh, 21d
+	jne line9
+	mov dh, 11d
+	mov dl, 90d
+	
+	;; the lines for bottom stacks are finished printing, now to invidually print each stack with
+	;; the proper number of facedown cards and the top card
+
+print_stack_7:
+	;; this needs to do the following:
+	;; search for card that belongs in the current position of this deck (looping from 0-?)
+	;; two modes:
+	;; Mode1	The first mode is automatically set.
+	;; 	This mode remains until a card is found that should be "Shown"
+	;; 	The first mode prints just tops of cards to signify face down cards
+	;; Mode2	this mode prints face up cards towards the top of the stack
+	;; 	This mode will continue until no more cards exist in the stack
+	;; 	This will simply print the next cards value (King of Spades)
+	;; 	They should be printed one on top of the other
+	;; as this subroutine will be used 7 times make sure it can be called for each stack
+
 	popdx
+	ret
+print_lower_stacks:
+	push ax
+	push bx
+	push cx
+	push dx
+	;; need to store the pile number somewhere
+	;; need to store the current card number somewhere this corresponds to the position in stack
+	mov ch, 7h		;the current stack - will iterate from 7-13
+	mov cl, 0d 		;current card number
+	mov bx, card		;current mem location (gets incremented by 2 each iteration) (from 152 - 256)
+	mov dh, 11d
+	mov dl, 5d
+findcard:
+	mov al, [bx]
+	and al, 11110000b
+	cmp al, ch
+	je stackmatch
+	inc bx
+	inc bx
+	cmp bx, card + 52
+	je next_stack
+	jmp findcard
+	
+stackmatch:	
+	mov al, [bx+1]
+	and al, 00011111b
+	cmp al, cl
+	je cardmatch
+	inc bx
+	inc bx
+	cmp bx, card + 52
+	je next_stack
+	jmp findcard
+
+cardmatch:	
+	mov ax, bx
+	call printcard
+	inc dh
+	inc dh
+	dec dl
+	jmp findcard
+
+next_stack:			;we have finished one stack, increment stack, if < 13 continue, else done
+	inc ch
+	cmp ch, eh
+	je finished
+	jmp findcard
+	
+finished:	
+	pop dx
+	pop cx
+	pop bx
+	pop ax
 	ret
 	
 .data:
