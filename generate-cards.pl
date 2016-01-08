e43321ca1ee51f947a5d4c830d9394dd8732b783
Brian Masney <masneyb@onstation.org>
2016-01-08 11:39:01 -0500

    added a few more cards back with the space savings

--- generate-cards.pl
+++ generate-cards.pl
@@ -58,7 +58,7 @@ print "first_card";
 my @pile_pointers;
 $pile_pointers[0] = "00000000";
 
-for (my $i = 0; $i < 5; $i++) {
+for (my $i = 0; $i < 16; $i++) {
 	my $val = pop @stack;
 
 	my $debug = '';
